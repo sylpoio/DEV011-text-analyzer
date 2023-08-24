c46d99cb7b49b793075f1a550385d0b4b5dbda94
sylpoio <syl.gomez.bio@gmail.com>
2023-08-24 09:18:46 -0600

    Jueves mañana

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+//en dónde voy a contar
+textArea=document.getElementsByName ("user-input")
+//Contar caracteres
