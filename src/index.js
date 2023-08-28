ab9adee4703006ea9cf80c857893697bc4cb9a0f
sylpoio <syl.gomez.bio@gmail.com>
2023-08-28 09:44:19 -0600

    jueves mañana

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import analyzer from './analyzer.js';
 
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-//en dónde voy a contar
-textArea=document.getElementsByName ("user-input")
+//de dónde voy a contar
+const textarea=document.querySelector("textarea[name='user-input']")
+textarea.addEventListener("keyup",()=>{
+    const resultado=analyzer.getWordCount(textarea.value)
+    console.log (resultado)
+
+})
 //Contar caracteres
+
+//botón reinicio
+const botonReiniciar=document.getElementById("reset-button")
+botonReiniciar.addEventListener('click', reiniciar)
+function reiniciar() {
+    location.reload();
+  
+}
