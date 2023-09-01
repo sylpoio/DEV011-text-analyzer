35e2168e13fba673da816fddad2ec5116fb89941
sylpoio <syl.gomez.bio@gmail.com>
2023-09-01 13:24:04 -0600

    Final

--- src/index.js
+++ src/index.js
@@ -6,34 +6,31 @@ import analyzer from './analyzer.js';
 const textarea=document.querySelector("textarea[name='user-input']")
 
 textarea.addEventListener("keyup",()=>{
-  const wordCountResult = document.getElementById("word-count");
-  wordCountResult.innerHTML = analyzer.getWordCount(textarea.value);
-    
-  const averageWordLengthResult = document.getElementById("word-length-average");
-  averageWordLengthResult.innerHTML = analyzer.getAverageWordLength(textarea.value);
+  const wordCountResult = document.querySelector("li[data-testid='word-count']")
+  
+  const averageWordLengthResult = document.querySelector("li[data-testid='word-length-average']");
+  const characterCountResult = document.querySelector("li[data-testid='character-count']");
+  const characterCountExcludingSpacesResult = document.querySelector("li[data-testid='character-no-spaces-count']");
+  const numberCountResult = document.querySelector("li[data-testid='number-count']");
+  const numberSumResult = document.querySelector("li[data-testid='number-sum']");
 
-  const characterCountResult = document.getElementById("character-count");
-  characterCountResult.innerHTML = analyzer.getCharacterCount(textarea.value);
-   
-  const characterCountExcludingSpacesResult = document.getElementById("character-no-spaces-count");
-  characterCountExcludingSpacesResult.innerHTML = analyzer.getCharacterCountExcludingSpaces(textarea.value);
-
-  const numberCountResult = document.getElementById("number-count");
-  numberCountResult.innerHTML = analyzer.getNumberCount(textarea.value);
-    
-  const numberSumResult = document.getElementById("number-sum");
-  numberSumResult.innerHTML = analyzer.getNumberSum(textarea.value);
+  wordCountResult.innerHTML= "Palabras: " + analyzer.getWordCount(textarea.value);
+  averageWordLengthResult.innerHTML = "Longitud media de palabras: " + analyzer.getAverageWordLength(textarea.value);
+  characterCountResult.innerHTML = " Caracteres: " + analyzer.getCharacterCount(textarea.value);
+  characterCountExcludingSpacesResult.innerHTML = "Caracteres excluyendo espacios y signos de puntuación: " + analyzer.getCharacterCountExcludingSpaces(textarea.value);
+  numberCountResult.innerHTML = "Números:" + analyzer.getNumberCount(textarea.value);
+  numberSumResult.innerHTML = "Suma de números:" + analyzer.getNumberSum(textarea.value);
 
   //botón reinicio
   const botonReiniciar=document.getElementById("reset-button")
   botonReiniciar.addEventListener('click', () => {
     textarea.value="";
-    wordCountResult.textContent="0"
-    averageWordLengthResult.textContent="0"
-    characterCountResult.textContent="0"
-    characterCountExcludingSpacesResult.textContent="0"
-    numberCountResult.textContent="0"
-    numberSumResult.textContent="0"
+    wordCountResult.textContent="Palabras: 0"
+    averageWordLengthResult.textContent="Longitud media de palabras: 0"
+    characterCountResult.textContent="Caracteres: 0"
+    characterCountExcludingSpacesResult.textContent="Caracteres sin espacios ni signos de puntuación: 0"
+    numberCountResult.textContent="Números: 0"
+    numberSumResult.textContent="Suma de números: 0"
   })
   
 })
